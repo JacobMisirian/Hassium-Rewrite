3871bc362ac3d480619c508ab9afcc6ca3d3ffed
Jacob Misirian <misirianrule@gmail.com>
2016-04-24 13:56:55 -0500

    Redid HassiumFileReader

--- src/Hassium/Runtime/StandardLibrary/IO/HassiumFileReader.cs
+++ src/Hassium/Runtime/StandardLibrary/IO/HassiumFileReader.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Text;
 
 using Hassium.Runtime.StandardLibrary.Types;
 
@@ -7,7 +8,7 @@ namespace Hassium.Runtime.StandardLibrary.IO
 {
     public class HassiumFileReader: HassiumObject
     {
-        public new BinaryReader Value { get; set; }
+        public BinaryReader BinaryReader { get; set; }
 
         public HassiumFileReader()
         {
@@ -18,7 +19,7 @@ namespace Hassium.Runtime.StandardLibrary.IO
         {
             HassiumFileReader hassiumFileReader = new HassiumFileReader();
 
-            hassiumFileReader.Value = new BinaryReader(new StreamReader(HassiumString.Create(args[0]).Value).BaseStream);
+            hassiumFileReader.BinaryReader = new BinaryReader(new StreamReader(HassiumString.Create(args[0]).Value).BaseStream);
             hassiumFileReader.Attributes.Add("endOfFile", new HassiumProperty(hassiumFileReader.get_EndOfFile));
             hassiumFileReader.Attributes.Add("length", new HassiumProperty(hassiumFileReader.get_Length));
             hassiumFileReader.Attributes.Add("position", new HassiumProperty(hassiumFileReader.get_Position));
@@ -26,42 +27,77 @@ namespace Hassium.Runtime.StandardLibrary.IO
             hassiumFileReader.Attributes.Add("readByte", new HassiumFunction(hassiumFileReader.readByte, 0));
             hassiumFileReader.Attributes.Add("readChar", new HassiumFunction(hassiumFileReader.readChar, 0));
             hassiumFileReader.Attributes.Add("readDouble", new HassiumFunction(hassiumFileReader.readDouble, 0));
+            hassiumFileReader.Attributes.Add("readInt16", new HassiumFunction(hassiumFileReader.readInt16, 0));
+            hassiumFileReader.Attributes.Add("readInt32", new HassiumFunction(hassiumFileReader.readInt32, 0));
+            hassiumFileReader.Attributes.Add("readInt64", new HassiumFunction(hassiumFileReader.readInt64, 0));
+            hassiumFileReader.Attributes.Add("readLine", new HassiumFunction(hassiumFileReader.readLine, 0));
             hassiumFileReader.Attributes.Add("readString", new HassiumFunction(hassiumFileReader.readString, 0));
 
             return hassiumFileReader;
         }
         public HassiumBool get_EndOfFile(VirtualMachine vm, HassiumObject[] args)
         {
-            return new HassiumBool(Value.BaseStream.Position < Value.BaseStream.Length);
+            return new HassiumBool(BinaryReader.BaseStream.Position >= BinaryReader.BaseStream.Length);
         }
         public HassiumDouble get_Length(VirtualMachine vm, HassiumObject[] args)
         {
-            return new HassiumDouble(Value.BaseStream.Length);
+            return new HassiumDouble(BinaryReader.BaseStream.Length);
         }
         public HassiumDouble get_Position(VirtualMachine vm, HassiumObject[] args)
         {
-            return new HassiumDouble(Value.BaseStream.Position);
+            return new HassiumDouble(BinaryReader.BaseStream.Position);
         }
         public HassiumBool readBool(VirtualMachine vm, HassiumObject[] args)
         {
-            return new HassiumBool(Value.ReadBoolean());
+            return new HassiumBool(BinaryReader.ReadBoolean());
         }
         public HassiumChar readByte(VirtualMachine vm, HassiumObject[] args)
         {
-            return new HassiumChar(Convert.ToChar(Value.ReadByte()));
+            return new HassiumChar(Convert.ToChar(BinaryReader.ReadByte()));
         }
         public HassiumChar readChar(VirtualMachine vm, HassiumObject[] args)
         {
-            return new HassiumChar(Value.ReadChar());
+            return new HassiumChar(BinaryReader.ReadChar());
         }
         public HassiumDouble readDouble(VirtualMachine vm, HassiumObject[] args)
         {
-            return new HassiumDouble(Value.ReadDouble());
+            return new HassiumDouble(BinaryReader.ReadDouble());
+        }
+        public HassiumInt readInt16(VirtualMachine vm, HassiumObject[] args)
+        {
+            return new HassiumInt(BinaryReader.ReadInt16());
+        }
+        public HassiumInt readInt32(VirtualMachine vm, HassiumObject[] args)
+        {
+            return new HassiumInt(BinaryReader.ReadInt32());
+        }
+        public HassiumInt readInt64(VirtualMachine vm, HassiumObject[] args)
+        {
+            return new HassiumInt(BinaryReader.ReadInt64());
+        }
+        public HassiumString readLine(VirtualMachine vm, HassiumObject[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            while (true)
+            {
+                if (get_EndOfFile(vm, args).Value)
+                    return new HassiumString(sb.ToString());
+                char one = readChar(vm, args).Value;
+                if (get_EndOfFile(vm, args).Value)
+                    return new HassiumString(sb.ToString());
+                char two = readChar(vm, args).Value;
+                if (one == '\r' && two == '\n')
+                    return new HassiumString(sb.ToString() + "\n");
+                else
+                {
+                    sb.Append(one);
+                    sb.Append(two);
+                }
+            }
         }
         public HassiumString readString(VirtualMachine vm, HassiumObject[] args)
         {
-            return new HassiumString(Value.ReadString());
+            return new HassiumString(BinaryReader.ReadString());
         }
     }
 }
-
