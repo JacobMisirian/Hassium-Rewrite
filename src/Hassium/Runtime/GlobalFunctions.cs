37ffb922ba89621991f2fa64575dcff93a985fa0
Jacob Misirian <misirianrule@gmail.com>
2016-08-21 02:03:16 -0500

    Added map and range global functions.

--- src/Hassium/Runtime/GlobalFunctions.cs
+++ src/Hassium/Runtime/GlobalFunctions.cs
@@ -12,8 +12,10 @@ namespace Hassium.Runtime
         {
             { "format", new HassiumFunction(format, -1) },
             { "input", new HassiumFunction(input, 0) },
+            { "map", new HassiumFunction(map, 2) },
             { "print", new HassiumFunction(print, -1) },
             { "println", new HassiumFunction(println, -1) },
+            { "range", new HassiumFunction(range, 1, 2) },
             { "type", new HassiumFunction(type, 1) }
         };
 
@@ -28,6 +30,17 @@ namespace Hassium.Runtime
         {
             return new HassiumString(Console.ReadLine());
         }
+        public static HassiumList map(VirtualMachine vm, params HassiumObject[] args)
+        {
+            HassiumList list = args[0].ToList(vm) as HassiumList;
+            HassiumList result = new HassiumList(new HassiumObject[0]);
+
+            for (int i = 0; i < list.List.Count; i++)
+                if (args[1].Invoke(vm, list.List[i]).ToBool(vm).Bool)
+                    result.add(vm, list.List[i]);
+
+            return result;
+        }
         public static HassiumObject print(VirtualMachine vm, params HassiumObject[] args)
         {
             foreach (var arg in args)
@@ -39,6 +52,25 @@ namespace Hassium.Runtime
             foreach (var arg in args)
                 Console.WriteLine(arg.ToString(vm).String);
             return HassiumObject.Null;
+        }
+        public static HassiumList range(VirtualMachine vm, params HassiumObject[] args)
+        {
+            int start, end;
+            switch (args.Length)
+            {
+                case 1:
+                    start = 0;
+                    end = (int)args[0].ToInt(vm).Int;
+                    break;
+                default:
+                    start = (int)args[0].ToInt(vm).Int;
+                    end = (int)args[1].ToInt(vm).Int;
+                    break;
+            }
+            HassiumList result = new HassiumList(new HassiumObject[0]);
+            while (start < end)
+                result.add(vm, new HassiumInt(start++));
+            return result;
         }
         public static HassiumTypeDefinition type(VirtualMachine vm, params HassiumObject[] args)
         {
