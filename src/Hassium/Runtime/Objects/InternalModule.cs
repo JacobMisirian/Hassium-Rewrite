33cfb25a0bbd5baa314c7a42b895bd1e4203be73
Jacob Misirian <misirianrule@gmail.com>
2016-08-22 17:05:11 -0500

    Added Reflection module

--- src/Hassium/Runtime/Objects/InternalModule.cs
+++ src/Hassium/Runtime/Objects/InternalModule.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Hassium.Runtime.Objects.IO;
 using Hassium.Runtime.Objects.Math;
 using Hassium.Runtime.Objects.Net;
+using Hassium.Runtime.Objects.Reflection;
 using Hassium.Runtime.Objects.Text;
 using Hassium.Runtime.Objects.Types;
 using Hassium.Runtime.Objects.Util;
@@ -20,12 +21,13 @@ namespace Hassium.Runtime.Objects
 
         public static Dictionary<string, InternalModule> InternalModules = new Dictionary<string, InternalModule>()
         {
-            { "IO",     new HassiumIOModule()       },
-            { "Math",   new HassiumMathModule()     },
-            { "Net",    new HassiumNetModule()      },
-            { "Text",   new HassiumTextModule()     },
-            { "Types",  new HassiumTypesModule()    },
-            { "Util",   new HassiumUtilModule()     }
+            { "IO",         new HassiumIOModule()           },
+            { "Math",       new HassiumMathModule()         },
+            { "Net",        new HassiumNetModule()          },
+            { "Reflection", new HassiumReflectionModule()   },
+            { "Text",       new HassiumTextModule()         },
+            { "Types",      new HassiumTypesModule()        },
+            { "Util",       new HassiumUtilModule()         }
         };
     }
 }
