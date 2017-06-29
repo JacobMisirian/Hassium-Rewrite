43066367576dd20033342067db09e432db24d43d
Jacob Misirian <misirianrule@gmail.com>
2017-06-28 23:23:01 -0600

    Updated dictionary type

--- src/Hassium/Runtime/Types/HassiumDictionary.cs
+++ src/Hassium/Runtime/Types/HassiumDictionary.cs
@@ -21,8 +21,20 @@ namespace Hassium.Runtime.Types
             foreach (var pair in initial)
                 Dictionary.Add(pair.Key, pair.Value);
 
+            AddAttribute("containsKey", containsKey, 1);
             AddAttribute(INDEX, Index, 1);
             AddAttribute(ITER, Iter, 0);
+            AddAttribute(STOREINDEX, StoreIndex, 2);
+        }
+
+        public HassiumBool containsKey(VirtualMachine vm, SourceLocation location, params HassiumObject[] args)
+        {
+            if (Dictionary.ContainsKey(args[0]))
+                return new HassiumBool(true);
+            foreach (var key in Dictionary.Keys)
+                if (key.EqualTo(vm, location, args[0]).Bool)
+                    return new HassiumBool(true);
+            return new HassiumBool(false);
         }
 
         public override HassiumObject Index(VirtualMachine vm, SourceLocation location, params HassiumObject[] args)
@@ -41,6 +53,15 @@ namespace Hassium.Runtime.Types
             foreach (var pair in Dictionary)
                 list.add(vm, location, new HassiumTuple(pair.Key, pair.Value));
             return list;
+        }
+
+        public override HassiumObject StoreIndex(VirtualMachine vm, SourceLocation location, params HassiumObject[] args)
+        {
+            if (containsKey(vm, location, args[0]).Bool)
+                Dictionary[args[0]] = args[1];
+            else
+                Dictionary.Add(args[0], args[1]);
+            return args[1];
         }
     }
 }
